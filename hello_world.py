84b932a4912a182a796ee8314ebc1aa9cc4c1092
andreashuebner <andreas.huebnerh@gmail.com>
2015-03-30 20:17:58 +0000

    Status before pushing to Heroku

--- hello_world.py
+++ hello_world.py
@@ -1,37 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/hello")
 def hello_world():
-    return "Hello World!"
+    return render_template('hello.html')
+    
 
-@app.route("/jedi/<first_name>/<last_name>")
-def hello_jedi(first_name,last_name):
-    fullName = first_name + " " + last_name
-    jediName = ""
-    jediName = last_name[0:3] + first_name[0:2]
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Your jedi name is {}.
-        </p>
-   """
-    return html.format(fullName,jediName)
+# @app.route("/jedi/<first_name>/<last_name>")
+# def hello_jedi(first_name,last_name):
+#     fullName = first_name + " " + last_name
+#     jediName = ""
+#     jediName = last_name[0:3] + first_name[0:2]
+#     html = """
+#         <h1>
+#             Hello {}!
+#         </h1>
+#         <p>
+#             Your jedi name is {}.
+#         </p>
+#    """
+#     return html.format(fullName,jediName)
 @app.route("/hello/<name>")
 def hello_person(name):
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Here's a picture of a kitten.  Awww...
-        </p>
-        <img src="http://placekitten.com/g/200/300">
-    """
-    return html.format(name.title())
+    return render_template('hello_name.html',name=name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
